ac56092df000d8768143539c27b07cbcac95a684
Jackson <146338884+jamarten291@users.noreply.github.com>
2025-02-24 18:49:08 +0100

    Update script.js
    variable names changed to english

--- Multiplication_Tables/js/script.js
+++ Multiplication_Tables/js/script.js
@@ -1,12 +1,12 @@
 document.querySelectorAll("input")[1].addEventListener("click", 
     () => {
-        let numero = parseInt(document.querySelector("#num").value)
-        let seccion = document.querySelectorAll("section")[0]
+        let number = parseInt(document.querySelector("#num").value)
+        let section = document.querySelectorAll("section")[0]
 
-        if (!isNaN(numero)){
-            if (numero >= 0 && numero <= 10){
-                seccion.innerHTML = "" // Clears the previous table so they don't get stacked
-                seccion.append(document.createElement("table"))
+        if (!isNaN(number)){
+            if (number >= 0 && number <= 10){
+                section.innerHTML = "" // Clears the previous table so they don't get stacked
+                section.append(document.createElement("table"))
 
                 document.querySelectorAll("table")[0].append(document.createElement("tbody"))
 
@@ -14,42 +14,42 @@ document.querySelectorAll("input")[1].addEventListener("click",
                     document.querySelectorAll("tbody")[0].append(document.createElement("tr"))
                     
                     for (let col = 0; col < 7; col++){
-                        let tdActual = document.createElement("td")
-                        document.querySelectorAll("tr")[i -1].append(tdActual)
+                        let currentTd = document.createElement("td")
+                        document.querySelectorAll("tr")[i - 1].append(currentTd)
 
                         // Creation of the input that receives numbers on each row of the table
-                        let inputNumero = document.createElement("input"); 
-                        inputNumero.type = "number"; 
-                        inputNumero.classList.add("numResultado")
-                        inputNumero.id = "input" + (i)
+                        let inputNumber = document.createElement("input"); 
+                        inputNumber.type = "number"; 
+                        inputNumber.classList.add("numResult")
+                        inputNumber.id = "input" + (i)
 
                         // This paragraph stores the "Correct!" or "Incorrect!" answer
                         let check = document.createElement("p")
-                        check.id = "resultado" + (i)
+                        check.id = "result" + (i)
 
-                        let botonNumero = document.createElement("input"); 
-                        botonNumero.type = "button"; 
-                        botonNumero.value="Comprobar"
-                        botonNumero.id = "submit" + (i)
-                        botonNumero.classList.add("botonResultado")
+                        let buttonNumber = document.createElement("input"); 
+                        buttonNumber.type = "button"; 
+                        buttonNumber.value = "Check"
+                        buttonNumber.id = "submit" + (i)
+                        buttonNumber.classList.add("buttonResult")
 
                         // This event listener checks if the answer is right
-                        botonNumero.addEventListener("click", ()=>{
+                        buttonNumber.addEventListener("click", ()=>{
                             // This grabs the value stored on the textbox from the same row as the clicked button
                             let inputUser = parseInt(document.getElementById("input" + (i)).value)
 
-                            document.getElementById("resultado" + (i)).textContent = 
-                            numero * (i) == inputUser ? "Correct!" : "Incorrect!"
+                            document.getElementById("result" + (i)).textContent = 
+                            number * (i) == inputUser ? "Correct!" : "Incorrect!"
                             }   
                         )
 
-                        // This ternary operator checks the column in the for loop, creating the structure of each row and using previously created elements like "inputNumero" or "check"
-                        tdActual.append(col == 0 ? numero : 
+                        // This ternary operator checks the column in the for loop, creating the structure of each row and using previously created elements like "inputNumber" or "check"
+                        currentTd.append(col == 0 ? number : 
                                         col == 1 ? " * " :
                                         col == 2 ? i :
                                         col == 3 ? " = " : 
-                                        col == 4 ? inputNumero :
-                                        col == 5 ? botonNumero :
+                                        col == 4 ? inputNumber :
+                                        col == 5 ? buttonNumber :
                                         check
                                     )
                     }
